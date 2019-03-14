8e7d340b1eb594406644170217bf35c72bf02756
ljl-os <594255598@qq.com>
2019-03-14 22:19:23 +0800

    文件格式化同步

--- src/main/java/com/ws/commons/algorithm/SequenceGenerate.java
+++ src/main/java/com/ws/commons/algorithm/SequenceGenerate.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
-
 /**
  * 序列号生成器：
  * 
@@ -25,8 +24,9 @@ public class SequenceGenerate {
 		if (CONFIG_FLAG) {
 			throw new RuntimeException("已经配置过了");
 		}
-		CONFIG_FLAG=true;
-		System.out.println("=============================================序列生成器已启动");
+		CONFIG_FLAG = true;
+		System.out.println(
+				"=============================================序列生成器已启动，应用编号：" + APP_NO + "   支持每秒并发量:" + (MAX_NO - 1));
 	}
 
 	public void setCps(Integer cps) {
@@ -41,6 +41,7 @@ public class SequenceGenerate {
 			APP_NO = appNo;
 		}
 	}
+
 	/**
 	 * 配置标记
 	 */
@@ -66,12 +67,13 @@ public class SequenceGenerate {
 	private static final Map<String, AtomicInteger> SEQUENCE_MAP = new HashMap<String, AtomicInteger>();
 
 	/**
-	 * 设置续接起始序号：
-	 * 使用场景：序号默认是从1开始，累计到cps（默认10w）后，或者服务重启后，又从1开始累计，
+	 * 设置续接起始序号： 使用场景：序号默认是从1开始，累计到cps（默认10w）后，或者服务重启后，又从1开始累计，
 	 * 若是累计到5w断了，重启服务需要从50001开始累计，则用此函数设置
 	 * 
-	 * @param sequenceKey 序列key
-	 * @param num         序号
+	 * @param sequenceKey
+	 *            序列key
+	 * @param num
+	 *            序号
 	 */
 	public static void setStartNum(String sequenceKey, int startNum) {
 		synchronized (SEQUENCE_MAP) {
@@ -82,8 +84,11 @@ public class SequenceGenerate {
 
 	/**
 	 * 初始化配置
-	 * @param cps   默认值100000，默认每秒最高并发量，也是最大序号，自增到该序号后从1开始重新自增
-	 * @param appNo 默认值0，应用编号
+	 * 
+	 * @param cps
+	 *            默认值100000，默认每秒最高并发量，也是最大序号，自增到该序号后从1开始重新自增
+	 * @param appNo
+	 *            默认值0，应用编号
 	 */
 	public static void initConfiguraion(int cps, String appNo) {
 		if (!SEQUENCE_MAP.isEmpty()) {
@@ -113,41 +118,43 @@ public class SequenceGenerate {
 
 	/**
 	 * 
-	 * @param sequenceKey 序列key，自定义一个key，每次获取都会在对应的key首次获取序列值的基础上递增
+	 * @param sequenceKey
+	 *            序列key，自定义一个key，每次获取都会在对应的key首次获取序列值的基础上递增
 	 * @return
 	 */
 	public static String nexId(String sequenceKey) {
 		return nexId(sequenceKey, SequenceGenerate.MAX_NO);
 	}
-	
-	public static LocalDateTime getDateTiemById(String id){
-		int year=Integer.parseInt("20"+id.substring(0, 2));
-		int month=Integer.parseInt(id.substring(2, 4));
-		int day=Integer.parseInt(id.substring(4, 6));
-		int hour=Integer.parseInt(id.substring(6, 8));
-		int minute=Integer.parseInt(id.substring(8, 10));
-		int second=Integer.parseInt(id.substring(10, 12));
-		
-		LocalDateTime ldt=LocalDateTime.of(year, month, day,hour, minute,second);
+
+	public static LocalDateTime getDateTiemById(String id) {
+		int year = Integer.parseInt("20" + id.substring(0, 2));
+		int month = Integer.parseInt(id.substring(2, 4));
+		int day = Integer.parseInt(id.substring(4, 6));
+		int hour = Integer.parseInt(id.substring(6, 8));
+		int minute = Integer.parseInt(id.substring(8, 10));
+		int second = Integer.parseInt(id.substring(10, 12));
+
+		LocalDateTime ldt = LocalDateTime.of(year, month, day, hour, minute, second);
 		return ldt;
 	}
-	
-	
+
 	/**
-	 *   时间标记的序列，因为目前的实现本来就是时间标记的序列，若今后扩展，那么 nexId 返回的将不一定是时间标记的序列
-	 * @param sequenceKey 序列key，自定义一个key，每次获取都会在对应的key首次获取序列值的基础上递增
+	 * 时间标记的序列，因为目前的实现本来就是时间标记的序列，若今后扩展，那么 nexId 返回的将不一定是时间标记的序列
+	 * 
+	 * @param sequenceKey
+	 *            序列key，自定义一个key，每次获取都会在对应的key首次获取序列值的基础上递增
 	 * @return
 	 */
 	public static String nexIdForTimeStamp(String sequenceKey) {
 		return nexId(sequenceKey, SequenceGenerate.MAX_NO);
 	}
 
-	
-
 	/**
 	 * 
-	 * @param sequenceKey 序列key，自定义一个key，每次获取都会在对应的key首次获取序列值的基础上递增
-	 * @param cps         每秒最高并发量
+	 * @param sequenceKey
+	 *            序列key，自定义一个key，每次获取都会在对应的key首次获取序列值的基础上递增
+	 * @param cps
+	 *            每秒最高并发量
 	 * @return
 	 */
 	public static String nexId(String sequenceKey, int cps) {
