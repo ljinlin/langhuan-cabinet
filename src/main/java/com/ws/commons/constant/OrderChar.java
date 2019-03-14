34ee8c2cecf71e770460433e606c55d14b6ec934
Jinlin Li <vn0wr5w@email.wal-mart.com>
2019-03-14 09:37:26 +0800

    新增排序sql关键字

---
+++ src/main/java/com/ws/commons/constant/OrderChar.java
@@ -0,0 +1,19 @@
+package com.ws.commons.constant;
+
+/**
+ * 
+ * @author  ljl·尘无尘
+ *
+ * Mar 14, 2019
+ */
+public  enum OrderChar {
+	
+DESC("DESC"),
+ASC("ASC");
+
+public final String code;
+
+OrderChar(String code) {
+	this.code = code;
+}
+}
